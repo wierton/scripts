40fe600a640fce54c7b950288c656b547413877c
141242068-ouxianfei <141242068@smail.nju.edu.cn>
2016-05-25 12:00:50 +0800

    git config

--- config.sh
+++ config.sh
@@ -2,3 +2,9 @@
 
 apt-get install git vim tmux
 cat vimrc >> /etc/vim/vimrc
+
+git config user.name=141242068-ouxianfei
+git config user.email=141242068@smail.nju.edu.cn
+git config core.editor=vim
+git config color.ui=true
+git config push.default=simple
