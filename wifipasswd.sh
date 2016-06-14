352666ebb4b23cf4e406d87fc66409fa5130c7ab
141242068-ouxianfei <141242068@smail.nju.edu.cn>
2016-06-14 23:29:43 +0800

    wifipasswd

---
+++ wifipasswd.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd /etc/NetworkManager/system-connections/
+
+printf "%20s - %s\n" "ssid" "passwd"
+for file in *; do
+	psk=`grep "psk=" "$file" 2>/dev/null |  sed "s/psk=//g"`
+	printf "%20s : " "$file"
+	if [ ! -r "$file" ];then
+		printf "\033[1;31mDenied\n\033[0m"
+	elif [ `grep -c "key-mgmt=" "$file"` -eq 0 ]; then
+		printf "\033[1;34mNone\n\033[0m"
+	elif [ "$psk" == "" ]; then
+		printf "\033[1;33mUnknown\n\033[0m"
+	else
+		printf "\033[1;32m$psk\n\033[0m"
+	fi
+done
