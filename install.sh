2c849cea8923d93327086d5a1bc686f2e8323b74
141242068-ouxianfei <141242068@smail.nju.edu.cn>
2019-03-13 11:19:43 +0800

    Added sudo for priviledged command

--- install.sh
+++ install.sh
@@ -56,68 +56,68 @@ use_tsinghua_source() {
 
 install_extra_cli_source() {
   # sbt
-  echo "deb https://dl.bintray.com/sbt/debian /" | tee -a /etc/apt/sources.list.d/sbt.list
-  apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
+  echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
+  sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
 }
 
 install_extra_gui_source() {
-  add-apt-repository -y ppa:umang/indicator-stickynotes &&
+  sudo add-apt-repository -y ppa:umang/indicator-stickynotes &&
   echo '
   deb http://ppa.launchpad.net/hzwhuang/ss-qt5/ubuntu xenial main
   deb-src http://ppa.launchpad.net/hzwhuang/ss-qt5/ubuntu xenial main
-  ' >> /etc/apt/sources.list # shadowsocks-qt5
+  ' | sudo tee -a /etc/apt/sources.list # shadowsocks-qt5
 }
 
 install_sbt() {
-  apt-get install -y sbt
+  sudo apt-get install -y sbt
 }
 
 install_verilator() {
-  apt-get install -y verilator
+  sudo apt-get install -y verilator
 }
 
 install_boost_libraries() {
-  apt-get install -y libboost-all-dev
+  sudo apt-get install -y libboost-all-dev
 }
 
 install_llvm_library() {
-  apt-get install -y libllvm6.0
+  sudo apt-get install -y libllvm6.0
 }
 
 install_clang_library() {
-  apt-get install -y libclang-6.0
+  sudo apt-get install -y libclang-6.0
 }
 
 install_readline_library() {
-  apt-get install -y libreadline-dev lib32readline-dev
+  sudo apt-get install -y libreadline-dev lib32readline-dev
 }
 
 install_parser_tools() {
- apt-get install -y flex bison
+ sudo apt-get install -y flex bison
 }
 
 install_useful_tool() {
-  apt-get install -y valgrind curl httpie cppman
+  sudo apt-get install -y valgrind curl httpie cppman
 }
 
 install_gcc_8() {
-  apt-get install -y gcc-8 g++-8
-  update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 50
-  update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-8 50
+  sudo apt-get install -y gcc-8 g++-8
+  sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 80
+  sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-8 80
 }
 
 install_gcc_multilib() {
-  apt-get install -y gcc-8-multilib g++-8-multilib
+  sudo apt-get install -y gcc-8-multilib g++-8-multilib
 }
 
 install_clang_6() {
-  apt-get install -y clang-6.0
-  update-alternatives --install /usr/bin/clang clang /usr/bin/clang-6.0 50
+  sudo apt-get install -y clang-6.0
+  sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-6.0 60
 }
 
 install_develop_essential() {
-  apt-get install -y tmux git zsh vim unzip unrar
-  apt-get install -y vim-gnome
+  sudo apt-get install -y tmux git zsh vim unzip unrar
+  sudo apt-get install -y vim-gnome
 }
 
 install_git_configure() {
@@ -153,7 +153,7 @@ post_install_oh_my_zsh() {
 }
 
 install_autojump() {
-  apt-get install -y autojump
+  sudo apt-get install -y autojump
   insert_to_file ". /usr/share/autojump/autojump.zsh" ${homedir}/.zshrc
   insert_to_file ". /usr/share/autojump/autojump.bash" ${homedir}/.bashrc
 }
@@ -164,7 +164,7 @@ install_clangformat() {
 
 install_numlockx() {
   # numlock
-  apt-get install -y numlockx
+  sudo apt-get install -y numlockx
   insert_to_file "numlockx on" /etc/profile
 }
 
@@ -189,20 +189,20 @@ install_tmux_conf_and_plugin() {
 }
 
 install_cmake() {
-  apt-get install -y cmake
+  sudo apt-get install -y cmake
 }
 
 install_gnu_mips_tool_chain() {
   # cross chain
-  apt-get install -y binutils-mips-linux-gnu
-  apt-get install -y cpp-mips-linux-gnu
-  apt-get install -y g++-mips-linux-gnu
-  apt-get install -y gcc-mips-linux-gnu
-  # apt-get install -y gcc-mips-linux-gnu-base
+  sudo apt-get install -y binutils-mips-linux-gnu
+  sudo apt-get install -y cpp-mips-linux-gnu
+  sudo apt-get install -y g++-mips-linux-gnu
+  sudo apt-get install -y gcc-mips-linux-gnu
+  # sudo apt-get install -y gcc-mips-linux-gnu-base
 }
 
 install_scala() {
-  apt-get install -y scala
+  sudo apt-get install -y scala
 }
 
 install_rust() {
@@ -217,41 +217,41 @@ install_rust() {
 
 install_cli_python() {
   # python libraries
-  apt-get install -y python-pip python3-pip python-virtualenv
-  update-alternatives --install /usr/bin/pip pip /usr/bin/pip3 10
-  update-alternatives --install /usr/bin/pip pip /usr/bin/pip2 20
-  pip2 install yd
+  sudo apt-get install -y python-pip python3-pip python-virtualenv
+  sudo update-alternatives --install /usr/bin/pip pip /usr/bin/pip3 10
+  sudo update-alternatives --install /usr/bin/pip pip /usr/bin/pip2 20
+  sudo pip2 install yd
 }
 
 install_qemu() {
   # qemu emulator
-  apt-get install -y qemu
+  sudo apt-get install -y qemu
 }
 
 install_sdl_library() {
-  apt-get install -y libsdl1.2-dev libsdl2-dev
-  apt-get install -y libgtk2.0-dev
-  apt-get install -y libgtk-3-dev
+  sudo apt-get install -y libsdl1.2-dev libsdl2-dev
+  sudo apt-get install -y libgtk2.0-dev
+  sudo apt-get install -y libgtk-3-dev
 
-  apt-get install -y libsdl1.2-dev:i386
-  apt-get install -y libsdl2-dev:i386
+  sudo apt-get install -y libsdl1.2-dev:i386
+  sudo apt-get install -y libsdl2-dev:i386
 }
 
 install_python_libraries() {
-  pip2 install numpy && pip3 install numpy
-  pip2 install scipy && pip3 install scipy
-  pip2 install tensorflow && pip3 install tensorflow
+  sudo pip2 install numpy && sudo pip3 install numpy
+  sudo pip2 install scipy && sudo pip3 install scipy
+  sudo pip2 install tensorflow && sudo pip3 install tensorflow
 }
 
 install_python_mysql() {
-  apt-get -y install mysql-server
-  apt-get -y install python-dev
-  apt-get -y install python-mysqldb
+  sudo apt-get -y install mysql-server
+  sudo apt-get -y install python-dev
+  sudo apt-get -y install python-mysqldb
 }
 
 install_docker() {
-  apt-get install -y docker.io
-  apt-get install -y docker-compose
+  sudo apt-get install -y docker.io
+  sudo apt-get install -y docker-compose
 }
 
 install_texlive() {
@@ -260,25 +260,25 @@ install_texlive() {
   # install synaptic
   # search for `texlive-full` `texlive` and `lyx` and then install all suggested packages
   # texmaker IDE in ubuntu
-  apt-get install -y texlive-full texlive texstudio
+  sudo apt-get install -y texlive-full texlive texstudio
 }
 
 install_ubuntu_unity_desktop_option=--no-redirect
 install_ubuntu_unity_desktop() {
-  apt-get install -y ubuntu-unity-desktop
+  sudo apt-get install -y ubuntu-unity-desktop
   dpkg-reconfigure lightdm
 }
 
 install_indicator_stickynotes() {
-  apt-get install -y indicator-stickynotes
+  sudo apt-get install -y indicator-stickynotes
 }
 
 install_shadowsocks() {
-  apt-get install -y shadowsocks-qt5
+  sudo apt-get install -y shadowsocks-qt5
 }
 
 install_proxychains4() {
-  apt-get install -y proxychains4
+  sudo apt-get install -y proxychains4
   sed -i 's/socks4  127.0.0.1 9050/socks5  127.0.0.1 1080/g' /etc/proxychains4.conf
   # sudo proxychains4 wget www.google.com
 }
@@ -303,14 +303,14 @@ install_google_chrome_stable() {(
 )}
 
 install_libpaxos() {(
-  apt-get install -y libevent-dev libmsgpack-dev
+  sudo apt-get install -y libevent-dev libmsgpack-dev
   git clone https://github.com/JiYou/cpaxos &&
 	cd cpaxos && mkdir output && cd output && cmake .. &&
 	make
 )}
 
 install_media_codecs() {
-  apt-get install -y ubuntu-restricted-extras
+  sudo apt-get install -y ubuntu-restricted-extras
 }
 
 run_install_instance() {(
@@ -337,7 +337,7 @@ install_cli() {
   use_tsinghua_source
   install_extra_cli_source
 
-  apt-get upgrade
+  sudo apt-get upgrade
 
   install verilator boost_libraries llvm_library \
 	clang_library readline_library parser_tools useful_tool \
@@ -349,7 +349,7 @@ install_cli() {
 
 install_gui() {
   install_extra_gui_source
-  apt-get upgrade
+  sudo apt-get upgrade
 
   install sdl_library python_libraries python_mysql docker \
 	texlive ubuntu_unity_desktop indicator_stickynotes \
@@ -370,4 +370,4 @@ quick_env
 # post_install_oh_my_zsh
 
 # autoremove
-# apt-get autoremove
+# sudo apt-get autoremove
